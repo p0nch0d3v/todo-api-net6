17a0d4fbcd2279bf98ed774d3554df8bde6b92b6
Poncho Dev <sneaky.koder@gmail.com>
2022-02-14 17:33:00 -0700

    WIP, Fix building, swagger info updated

--- TodoMinimalAPI/Program.cs
+++ TodoMinimalAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using TodoMinimalAPI;
 using TodoMinimalAPI.Data;
 
@@ -10,7 +11,13 @@ builder.Services.AddScoped<ITodoRepository, TodoRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { 
+            Title = "Todo API", 
+            Version = "v1" 
+        });
+    });
 
 var app = builder.Build();
 
